3f96d84b71b0b9909089479d752275ded83e76ea
nopisos <77279082+nopisos@users.noreply.github.com>
2021-01-15 09:27:34 +0300

    f

---
+++ Assets/scripts/ObjectCreator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private Vector3[] _points;
+    [SerializeField] private int _maxQuantity;
+    [SerializeField] private float _createTime;
+
+    private float _time;
+    private int _quantity = 0;
+
+    void Start()
+    {
+        _time = _createTime;
+    }
+
+    void Update()
+    {       
+        _time += Time.deltaTime;
+        if (_quantity != _maxQuantity && _time >= _createTime)
+        {
+            Instantiate(_prefab, _points[Random.Range(0, _points.Length)], Quaternion.identity);
+            _quantity++;
+            _time = 0;
+        }
+    }
+}
---
+++ Assets/scripts/RandomMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMove : MonoBehaviour
+{
+    [Range(1,5)]
+    [SerializeField] private float _speed;
+    
+    private Transform _transform;
+    private Vector2 _target;
+    private float _time = 5;
+
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        _time += Time.deltaTime;
+        _transform.Translate(_target*_speed*Time.deltaTime);
+        if(_time > 4)
+        {
+            _target.x = Random.Range(-1f, 1f);
+            _target.y = Random.Range(-1f, 1f);
+            _time = 0;
+        }
+    }
+}
