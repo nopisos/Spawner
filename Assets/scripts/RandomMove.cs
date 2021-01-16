78759d4dcb448defa10333bac759c9f0b1c4c5e1
nopisos <77279082+nopisos@users.noreply.github.com>
2021-01-16 13:46:27 +0300

    f

--- Assets/scripts/RandomMove.cs
+++ Assets/scripts/RandomMove.cs
@@ -6,6 +6,7 @@ public class RandomMove : MonoBehaviour
 {
     [Range(1,5)]
     [SerializeField] private float _speed;
+    [SerializeField] private float _spawnTime;
     
     private Transform _transform;
     private Vector2 _target;
@@ -20,7 +21,7 @@ public class RandomMove : MonoBehaviour
     {
         _time += Time.deltaTime;
         _transform.Translate(_target*_speed*Time.deltaTime);
-        if(_time > 4)
+        if(_time > _spawnTime)
         {
             _target.x = Random.Range(-1f, 1f);
             _target.y = Random.Range(-1f, 1f);
