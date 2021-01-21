c0ff8467b17504a44fce3699c1f371aba8fa097c
nopisos <77279082+nopisos@users.noreply.github.com>
2021-01-21 16:24:18 +0300

    k

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Spawner");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Additive);
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
